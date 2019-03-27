3ebb6ff14313ed80a6e351eb5b4e73503775d838
unknown <wangpeng04@ppdai.com>
2019-03-27 16:42:04 +0800

    add

---
+++ src/@cool/asker/README.md
@@ -0,0 +1,6 @@
+
+
+### 封装request 提供统一API  Asker
+user request---> node层---->Asker
+
+透传定义
--- src/middleware/listen.js
+++ src/middleware/listen.js
@@ -1,9 +1,26 @@
 
 const address = require('ip').address()
+const chalk = require('chalk')
+const figlet = require('figlet')
+
+function printLogo (Log) {
+  console.log(
+    chalk.blue(
+      figlet.textSync('cooL', {
+        font: 'slant',
+        horizontalLayout: 'full',
+        verticalLayout: 'full'
+      })
+    )
+  )
+}
 
 module.exports = async app => {
   const port = process.env.PORT || 3000
   app.listen(port, () => {
-    app.context.logger.info(`mode: ${process.env.NODE_ENV}---server on: http://${address}:${port}`)
+    app.context.logger.info(`mode: ${process.env.NODE_ENV}`)
+
+    printLogo()
+    console.log(`server on: http://${address}:${port}`)
   })
 }
