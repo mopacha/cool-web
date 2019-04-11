1b73b64d91dfc037215548d2a32fd15eae316ac8
unknown <wangpeng04@ppdai.com>
2019-04-11 10:59:00 +0800

    add

--- src/app.config.js
+++ src/app.config.js
@@ -1,17 +1,8 @@
 const config = require('@coofy/config')
 
 module.exports = {
-  // 模板变量全局配置
-  views: {
-    staticDomain: config.staticDomain,
-    staticHostUrl: `${config.staticDomain}${config.staticContextPath}`
-  },
-
-  // cool build
-  staticAssets: {
-    publicPath: `${config.staticDomain}${config.staticContextPath}`
-  },
-
+  staticDomain: config.staticDomain,
+  staticUrl: `${config.staticDomain}${config.staticContextPath}`,
   // 透传
   proxys: {
     demo: {
