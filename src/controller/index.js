680cf2e9849b969572e7ecb1aa2d9a998caf1ee7
moofyu <moofyu@163.com>
2019-04-14 23:43:57 +0800

    router

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,6 +1,12 @@
 const CoolService = require('../service/cool-service')
+const router = require('@coofy/koafy').router()
+const {
+  Get
+} = router
 
+@Controller('/')
 class Pages {
+  @Get('/demo')
   async demo (ctx, _next) {
     const title = 'demo'
     console.log('call demo')
@@ -9,6 +15,7 @@ class Pages {
     })
   }
 
+  @Post('/login')
   async login (ctx, _next) {
     const data = await CoolService.login(ctx, ctx.request.body)
     ctx.body = data
